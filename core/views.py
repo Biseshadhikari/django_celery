7a23eac687e55e51b2c868cc044bf0e162376e3e
Bisesh Adhikari <adhikaribisesh1590@gmail.com>
2024-03-01 15:01:18 +0545

    shared_task

--- core/views.py
+++ core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from project.celery import add
+# from project.celery import add
+from core.tasks import add
 # Create your views here.
 
 
 def home(request):
     x = add.delay(5,10)
-    # print(x)
 
     return render(request,'index.html',{'x':x})
